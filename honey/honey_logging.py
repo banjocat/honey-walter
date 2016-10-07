10a180e34f56a8109acaa4d3c164bbd78cc0b218
Jack Muratore <jackmuratore@gmail.com>
2016-10-07 00:31:11 -0400

    Add comments in honey_logging.py

--- honey/honey_logging.py
+++ honey/honey_logging.py
@@ -6,10 +6,11 @@ import logging
 
 from twisted.python import log
 
-
+# Links twisted logging with python logging
 _observer = log.PythonLoggingObserver()
 _observer.start()
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
+# Creates the logger to use to logstash
 stash = logging.getLogger('python-logstash-logger')
 stash.addHandler(logstash.TCPLogstashHandler('logstash', 5000))
