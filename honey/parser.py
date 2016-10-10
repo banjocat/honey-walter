60b93f23f4f33dc260afcf8f55c1e3cc05356513
Jack Muratore <jackmuratore@gmail.com>
2016-10-10 02:42:41 -0400

    Add fun package commands

--- honey/parser.py
+++ honey/parser.py
@@ -3,13 +3,12 @@ from config import CONFIG
 
 def parse_input(line):
     line_list = line.split(' ')
+    # Not sure what to do with args yet
     command, args = line_list[0], line_list[1:]
     command_list = CONFIG['simple_commands']
-    output = command_list.get(command)
-    if not output:
-        output = not_found(command)
+    output = command_list.get(command, _not_found(command))
     return output
 
 
-def not_found(command):
+def _not_found(command):
     return "{0}: command not found".format(command)
