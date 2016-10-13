58915fa25e41b89560cb5cfbaa256a16903d56b6
Jack Muratore <jackmuratore@gmail.com>
2016-10-13 00:24:52 -0400

    Move to port 22

--- fabfile.py
+++ fabfile.py
@@ -3,14 +3,14 @@ from fabric.api import settings, hosts, run, put
 
 @hosts('jackmuratore.com')
 def deploy():
-    with settings(user='root'):
+    with settings(user='root', port=2222):
         put('./puppet', '/tmp')
         run('puppet apply --modulepath /tmp/puppet/modules:/etc/puppet/modules /tmp/puppet/init.pp')
 
 
 @hosts('jackmuratore.com')
 def install_puppet():
-    with settings(user='root'):
+    with settings(user='root', port=2222):
         run('apt-get install -y puppet')
         run('/etc/init.d/puppet stop')
         run('puppet module install garethr-docker')
